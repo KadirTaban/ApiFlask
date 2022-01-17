ce776a3a897fa1556efdc35d3f510314f9bd6ecf
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2022-01-17 10:47:01 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,20 +4,20 @@ import pyrebase #pip install pyrebase
 
 
 firebaseconfig = {
-    "apiKey": "AIzaSyCjLiNVCxHNggIaqVaHFSFKn2Uf_UiOH2k",
-    "authDomain": "flaskapi-bcd20.firebaseapp.com",
-    "projectId": "flaskapi-bcd20",
-    "storageBucket": "flaskapi-bcd20.appspot.com",
-    "messagingSenderId": "1037291710285",
-    "databaseURL": "https://flaskApi.firebaseio.com",
-    "appId": "1:1037291710285:web:70ee99159c2fd23c9a7904",
-    "measurementId": "G-QXM50GE189"
+    "apiKey": "",
+    "authDomain": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "databaseURL": "",
+    "appId": "",
+    "measurementId": ""
 }
 firebase = pyrebase.initialize_app(firebaseconfig)
 firebase_db = firebase.database()
 
 auth = firebase.auth()
 
-cred = credentials.Certificate("/Users/mountainlabs/apiWithFlask/src/config.json")
+cred = credentials.Certificate("config.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
